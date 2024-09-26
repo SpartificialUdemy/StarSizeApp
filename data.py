a84feab58cc2c0f956a5bd44e47fa9a2aa880754
SpartificialUdemy <instructors@spartificial.com>
2024-09-27 03:05:53 +0530

    Instructions added to use this file

--- data.py
+++ data.py
@@ -1,14 +1,33 @@
+"""
+Run this file to generate your own data
+- Just change the number of samples as per your wish and run this file
+- You will have a csv file ready with the name "input_star_data.csv" 
+"""
+
+# Change the number of samples based on how much data you want to generate
+N_SAMPLES = 500
+
+# Imports
 import numpy as np
 import pandas as pd
 
+##############################################################################
+
+""" DO NOT CHANGE THE CODE BELOW """
+
+"""
+Training was done based on this model
+So if you change the code below, it may not work as expected
+"""
+
+##############################################################################
 
 # Define a seed value (other than 100 as 100 was used for training)
 np.random.seed(5007) 
 
-# Input Data for n samples
-n_samples = 500
-X_test = 3*np.random.rand(n_samples,1)
-y_test = 9 + 2*X_test + np.random.rand(n_samples,1)
+# Generate Data
+X_test = 3*np.random.rand(N_SAMPLES,1)
+y_test = 9 + 2*X_test + np.random.rand(N_SAMPLES,1)
 
 # Convert arrays into dict
 dict_info = {'Brightness' : X_test.reshape(-1,),
