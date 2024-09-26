f51f747bc7757ffdedea67d4fe3e67b3b089b2c3
SpartificialUdemy <instructors@spartificial.com>
2024-09-27 00:10:27 +0530

    commented code

--- data.py
+++ data.py
@@ -5,12 +5,15 @@ import pandas as pd
 # Define a seed value (other than 100 as 100 was used for training)
 np.random.seed(5007) 
 
-# Test data
-X_test = 3*np.random.rand(500,1)
-y_test = 9 + 2*X_test + np.random.rand(500,1)
+# Input Data for n samples
+n_samples = 500
+X_test = 3*np.random.rand(n_samples,1)
+y_test = 9 + 2*X_test + np.random.rand(n_samples,1)
 
+# Convert arrays into dict
 dict_info = {'Brightness' : X_test.reshape(-1,),
              'True Size' : y_test.reshape(-1,)}
 
+# Convert dict to pandas dataframe and save the csv
 input_df = pd.DataFrame(dict_info)
 input_df.to_csv('input_star_data.csv', index=False)
